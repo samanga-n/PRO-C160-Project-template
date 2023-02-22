09ff51f18702de8fc331a99c6843510aaf02db58
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2023-02-22 19:39:34 +0530

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -27,27 +27,15 @@ AFRAME.registerComponent("cursor-listener", {
   },
   handleMouseEnterEvents: function() {
     // Mouse Center Events
-    this.el.addEventListener("mouseenter", () => {
-      const placeContainer = document.querySelector("#places-container");
-      const { state } = placeContainer.getAttribute("tour");
-      if (state === "places-list") {
-        this.handlePlacesListState();
-      }
-    });
+   
   },
+  
   handlePlacesListState: function() {
-    const id = this.el.getAttribute("id");
-    const placesId = ["place-home", "place-garden", "place-main_gate"];
-    if (placesId.includes(id)) {
-      const placeContainer = document.querySelector("#places-container");
-      placeContainer.setAttribute("cursor-listener", {
-        selectedItemId: id
-      });
-      this.el.setAttribute("material", {
-        opacity: 1
-      });
-    }
+   // complete the function
+    
   },
+  
+  
   handleMouseLeaveEvents: function() {
     // Mouse Leave Events
     this.el.addEventListener("mouseleave", () => {
